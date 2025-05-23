7e96f5e9d8bbc6d5965d31fe2f2833b7d265d9d2
Tom Zacharoff <warelock2000@protonmail.com>
2025-05-23 14:38:31 -0700

    Fixed logging and added log rotation

--- create_vault_storage.sh
+++ create_vault_storage.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -
 sudo mkdir -p vault-node1/data/
 sudo mkdir -p vault-node1/logs/
-sudo chown -R 100:100 vault-node1/
-sudo chmod -R 770 vault-node1/
+sudo chown -R 100:1000 vault-node1/
+sudo chmod -R 700 vault-node1/
--- enable_vault_logging.sh
+++ enable_vault_logging.sh
@@ -1,2 +1,4 @@
 #!/bin/bash -
 vault audit enable file file_path=/vault/logs/vault_audit.log
+sed -i "s|^.*vault_audit.log|$(pwd)/vault-node1/logs/vault_audit.log|" vault_audit
+sudo cp vault_audit /etc/logrotate.d/vault_audit
